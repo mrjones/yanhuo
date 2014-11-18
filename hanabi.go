d49ea1d804fb3995c8a9c392505a1a1e4de7c0d7
Matt Jones <jonesmr@gmail.com>
2014-11-18 23:08:47 +0900

    basic action structs

--- hanabi.go
+++ hanabi.go
@@ -7,26 +7,27 @@ import (
 	"time"
 )
 
-type color int8
-type value int8
+type Color int8
+type Value int8
+type PlayerID int8
+type HandIndex int8
 
 const (
-	WHITE color = iota
+	WHITE Color = iota
 	RED
 	BLUE
 	YELLOW
 	GREEN
-
 )
 
-var ALL_COLORS = []color{WHITE, RED, BLUE, YELLOW, GREEN}
+var ALL_COLORS = []Color{WHITE, RED, BLUE, YELLOW, GREEN}
 
 type colorInfo struct {
 	fullName string
 	shortName string
 }
 
-var COLOR_INFOS = map[color]colorInfo {
+var COLOR_INFOS = map[Color]colorInfo {
 	WHITE: colorInfo{fullName: "WHITE", shortName: "W"},
 	RED: colorInfo{fullName: "RED", shortName: "R"},
 	BLUE: colorInfo{fullName: "BLUE", shortName: "B"},
@@ -35,7 +36,7 @@ var COLOR_INFOS = map[color]colorInfo {
 }
 
 
-var VALUE_COUNTS = map[value]int{
+var VALUE_COUNTS = map[Value]int{
 	1: 3,
 	2: 2,
 	3: 2,
@@ -51,6 +52,39 @@ var INITIAL_CARDS = map[int]int {
 	5: 4,
 }
 
+type GiveInformationAction struct {
+	Player PlayerID
+
+	// Exactly one of color or value must be non-null
+	Color *Color
+	Value *Value
+}
+
+type DiscardAction struct {
+	// The index of the card to discard
+	Index HandIndex
+}
+
+type PlayAction struct {
+	// The index of the card to discard
+	Index HandIndex
+}
+
+type Action struct {
+	// Exactly one one must be non-null
+	GiveInformation *GiveInformationAction
+	Discard *DiscardAction
+	Play *PlayAction
+}
+
+type Player interface {
+	// map player -> []card
+	// Act
+	Act(otherPlayersCards map[PlayerID][]Card, blueTokens int, redTokens int) Action
+
+	ObserveAction(actor PlayerID, action Action)
+}
+
 func main() {
 	fmt.Println("Hello, world!")
 	
@@ -61,40 +95,42 @@ func main() {
 		log.Fatal(err)
 	}
 
-	for i := 0; i < 4; i++ {
+	for i := PlayerID(0); i < 4; i++ {
 		fmt.Printf("PLAYER %d\n", i)
 		displayDeck(state.heldCards[i])
 	}
-
 }
 
 
 
-type card struct {
-	value value
-	color color
+type Card struct {
+	Value Value
+	Color Color
 }
 
 type gameState struct {
-	drawPile []card
-	heldCards map[int][]card
+	drawPile []Card
+	heldCards map[PlayerID][]Card
+
+	redTokens int  // bad plays
+	blueTokens int  // available information
 }
 
-func initialDraw(deck []card, numPlayers int) (*gameState, error) {
+func initialDraw(deck []Card, numPlayers int) (*gameState, error) {
 	cardsPerPlayer, ok := INITIAL_CARDS[numPlayers]
 	if !ok {
 		return nil, fmt.Errorf("Invalid number of players: %d", numPlayers)
 	}
 
-	state := &gameState{heldCards: make(map[int][]card), drawPile: []card{}}
+	state := &gameState{heldCards: make(map[PlayerID][]Card), drawPile: []Card{}}
 
-	for p := 0; p < numPlayers; p++ {
-		state.heldCards[p] = []card{}
+	for p := PlayerID(0); int(p) < numPlayers; p++ {
+		state.heldCards[p] = []Card{}
 	}
 
 	drawCount := 0
 	for c := 0; c < cardsPerPlayer; c++ {
-		for p := 0; p < numPlayers; p++ {
+		for p := PlayerID(0); int(p) < numPlayers; p++ {
 			// TODO(mrjones): bounds check
 			state.heldCards[p] = append(state.heldCards[p], deck[drawCount])
 			drawCount++
@@ -105,16 +141,16 @@ func initialDraw(deck []card, numPlayers int) (*gameState, error) {
 	return state, nil
 }
 
-func displayDeck(deck []card) {
+func displayDeck(deck []Card) {
 	for _, card := range(deck) {
-		fmt.Printf("color: %s, value: %d\n", COLOR_INFOS[card.color].fullName, card.value)
+		fmt.Printf("color: %s, value: %d\n", COLOR_INFOS[card.Color].fullName, card.Value)
 	}
 }
 
-func shuffle(in []card) []card {
+func shuffle(in []Card) []Card {
 	rand.Seed(time.Now().UTC().UnixNano())
 
-	out := []card{}
+	out := []Card{}
 	for _, i := range rand.Perm(len(in)) {
 		out = append(out, in[i])
 	}
@@ -122,12 +158,12 @@ func shuffle(in []card) []card {
 	return out
 }
 
-func createDeck() []card {
-	cards := []card{}
+func createDeck() []Card {
+	cards := []Card{}
 	for color, _ := range(COLOR_INFOS) {
 		for value, count := range(VALUE_COUNTS) {
 			for i := 0; i < count; i++ {
-				cards = append(cards, card{value: value, color: color})
+				cards = append(cards, Card{Value: value, Color: color})
 			}
 		}
 	}
